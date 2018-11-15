6e0d22b907f1c18bd24ed4b92d7bbf67539d4567
Andy Babic <ababic@rkh.co.uk>
2018-11-15 15:41:30 +0000

    Drop __stable_version__ and bump to 1.3a

--- healtheintent_api/__version__.py
+++ healtheintent_api/__version__.py
@@ -1,8 +1,7 @@
 __title__ = 'healtheintent-api-python'
 __description__ = "A simple API wrapper to help us perform commonly required actions in Cerner's various HealthEIntent REST APIs."
 __url__ = 'https://bitbucket.org/rkhleics/healtheintent-api-python/'
-__version__ = '1.2'
-__stable_version__ = '1.2'
+__version__ = '1.3a'
 __author__ = 'Andy Babic'
 __author_email__ = 'andyjbabic@gmail.com'
 __license__ = 'MIT'
